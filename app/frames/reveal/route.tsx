fbedce0bc6710562781aaf710ce51ddd66b73843
FREAK <freaks.bootcamp@gmail.com>
2024-11-15 21:48:36 +0900

    refactor: update profile image process

--- app/frames/reveal/route.tsx
+++ app/frames/reveal/route.tsx
@@ -6,12 +6,12 @@ const handleRequest = frames(async (ctx) => {
   // コンテキストをコンソールログに出力
   console.log("Context: %o", ctx);
 
-  // コンテキストから profileImage のURLを取得
-  const profileImageUrl: string | undefined = ctx.message?.requesterUserData?.profileImage;
+  // 変数 profileImageUrl の値として profileImage が取得できた場合は profileImage の値を、取得できなかった場合は unavailable.png を代入
+  const profileImageUrl = ctx.message?.requesterUserData?.profileImage || `${appURL()}/unavailable.png??`;
 
-  // プロフィール画像が取得できた場合に呼び出すコンポーネント
-  const ImageAvailable = () => {
-    return (
+  // フレームデータを返す
+  return {
+    image: (
       <div
         tw="flex w-full h-full pt-40 justify-center items-center"
         style={{
@@ -21,17 +21,7 @@ const handleRequest = frames(async (ctx) => {
       >
         <img tw="rounded-full" src={profileImageUrl} width="600" height="600" alt="" />
       </div>
-    );
-  };
-
-  // プロフィール画像が取得できなかった場合に呼び出すコンポーネント
-  const ImageUnavailable = () => {
-    return <img src={`${appURL()}/unavailable.png`} width="1146" height="1146" alt="" />;
-  };
-
-  // フレームデータを返す
-  return {
-    image: profileImageUrl ? <ImageAvailable /> : <ImageUnavailable />,
+    ),
     imageOptions: {
       aspectRatio: "1:1",
     },
